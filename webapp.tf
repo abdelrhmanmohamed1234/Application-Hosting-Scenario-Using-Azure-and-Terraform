6f8a9550b6912e4ecace5aa451041e9e28565ed2
Abdelrhman Mohamed <99396593+abdelrhmanmohamed1234@users.noreply.github.com>
2026-01-31 07:24:26 +0200

    Add Azure Service Plan and Linux Web App resources

--- webapp.tf
+++ webapp.tf
@@ -1,24 +1,24 @@
 # Admin App Service Plan (Linux – Basic Tier)
-# resource "azurerm_service_plan" "admin" {
-#   name                = "admin-service-plan"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#
-#   os_type  = "Linux"
-#   sku_name = "B1"
-# }
+ resource "azurerm_service_plan" "admin" {
+   name                = "admin-service-plan"
+   location            = var.location
+   resource_group_name = azurerm_resource_group.rg.name
 
-# Admin Linux Web App
-# resource "azurerm_linux_web_app" "admin" {
-#   name                = "admin-webapp"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   service_plan_id     = azurerm_service_plan.admin.id
-#
-#   site_config {
-#     always_on = true
-#   }
-# }
+   os_type  = "Linux"
+   sku_name = "B1"
+ }
+
+ Admin Linux Web App
+ resource "azurerm_linux_web_app" "admin" {
+   name                = "admin-webapp"
+   location            = var.location
+   resource_group_name = azurerm_resource_group.rg.name
+   service_plan_id     = azurerm_service_plan.admin.id
+
+   site_config {
+     always_on = true
+   }
+ }
 
 
 
